624f54ec0fb954e35c6c40607af5b32cba6d1920
Lamine Sall <salllaminee11@gmail.com>
2025-03-04 20:56:53 -0500

    Ajout de la fonction AddTask

--- model/appManager.js
+++ model/appManager.js
@@ -4,3 +4,16 @@ const tasks  = [];
 // Fonction pour recuperer toutes les tâches
 export const getAllTasks = ()=> tasks;
  
+// Fonction pour ajouter une tâche
+export const AddTask = (task)=>{
+    const newTask = {
+        id: tasks.length + 1,
+        titre,
+        description,
+        priorité,
+        statut: "À faire",
+        dateLimite,
+        historique: []
+    };
+    tasks.push(newTask);
+}
