4ae4b0e413f35db3a240d08e8843087adee0382b
Ab-Taha <aitbella.mtaha@gmail.com>
2025-03-06 13:35:34 -0500

    Ajout de la route pour ajouter une tache

--- routes.js
+++ routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import { getAllTasks, AddTask, UpdateTaskStatus, UpdateTask, deleteTask } from "./model/appManager.js";
 
 const router = Router();
 
-//Definition des routes
-/*  ............. */
+// Ajouter une tâche
+router.post("/api/todo", (request, response) => {
+    const tacheEntrée = request.body;
+    try {
+        const NouvelleTache = AddTask(tacheEntrée);
+        response
+            .status(200)
+            .json({ NouvelleTache, message: "Tâche ajoutée avec succès" });
+    } catch (error) {
+        response.status(400).json({ error: error.message });
+    }
+});
 
 export default router;
