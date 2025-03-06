dd3b6b875f5df31f6ce6ab46bc82e19c2bfb8416
Lamine Sall <salllaminee11@gmail.com>
2025-03-06 14:03:58 -0500

    Ajout de la route pour modifier le statut d'une tâche

--- routes.js
+++ routes.js
@@ -15,5 +15,22 @@ router.post("/api/todo", (request, response) => {
         response.status(400).json({ error: error.message });
     }
 });
+// Modifier le statut d'une tâche
+router.patch("/api/task/:id/statut", (request, response) => {
+    try {
+        const id = parseInt(request.params.id, 10);
+        const { statut } = request.body;
+        const updatedTask = UpdateTaskStatus(id, statut);
+
+        if (updatedTask) {
+            response.status(200).json({ updatedTask, message: "Statut mis à jour avec succès" });
+        } else {
+            response.status(404).json({ message: "Tâche non trouvée" });
+        }
+    } catch (error) {
+        response.status(400).json({ error: error.message });
+    }
+});
+
 
 export default router;
